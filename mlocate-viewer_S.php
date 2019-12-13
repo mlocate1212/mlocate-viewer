5c893a587643319483cd11bc6897290002e48719
mlocate1212 <58812863+mlocate1212@users.noreply.github.com>
2019-12-13 19:58:04 +0900

    Update mlocate-viewer_S.php

--- mlocate-viewer_S.php
+++ mlocate-viewer_S.php
@@ -21,10 +21,28 @@ $fp = popen("blkls -A /dev/sda1", "r");
 
 	//preg_match_all('/[\x00{4}](?<timestamp>.{16})[\0x00{4}](?<parent>.*)[\0x00]/', $data, $out, PREG_OFFSET_CAPTURE);
 	preg_match_all('/[\x00\x00\x00\x00](?<timestamp>.{16})[\0x00\x00\x00\x00](?<entry>.*)[\0x00\0x02]/', $data, $out, PREG_SET_ORDER);
-	echo $out[0][timestamp];
+
+	$time_dir = unpack('N4TIME/Z*dir', $out[0]);
+	$timestamp = $time_dir[TIME2];
+	$nanosec = $time_dir[TIME3];
+	$dir = $time_dir[dir];
+	$dateString = date("Y-m-d H:i:s", $timestamp).$nanosec;
+	
+	echo $dir." ==> ".$dateString;
+  
+	$subdir = substr_replace($data_arr[$i], '', 0, strlen($dir)+16); 
+	
+		$a = preg_replace('[\x00\x01]', "\n [D]", $subdir);
+		$c = preg_replace('[\x00\x00]', "\n [F]", $a);
+
+		
+	echo $c;
+
+	echo "\n\n";	
 	echo $out[0][entry];
 	
 	flush();
+		
 }
 
 ?>
